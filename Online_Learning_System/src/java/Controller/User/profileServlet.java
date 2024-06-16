bfe2a7614c11554cba50647492ed043709dcbdfe
C4tTu0n9 <tuongpche171654@fpt.edu.vn>
2024-06-16 19:11:47 +0700

    TuongPCHE171654 fix back-end insert img avatar profile

--- Online_Learning_System/src/java/Controller/User/profileServlet.java
+++ Online_Learning_System/src/java/Controller/User/profileServlet.java
@@ -7,19 +7,27 @@ package Controller.User;
 import Dal.AccountDAO;
 import Model.Account;
 import Model.Profile;
+import com.oracle.wls.shaded.org.apache.xalan.templates.AVT;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.Part;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import org.apache.http.client.fluent.Response;
 
 /**
  *
  * @author tuong
  */
+@MultipartConfig
 public class profileServlet extends HttpServlet {
 
     /**
@@ -82,11 +90,12 @@ public class profileServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        PrintWriter o = response.getWriter();
         HttpSession session = request.getSession();
-
-        String avt = request.getParameter("avt") == null ? "" : request.getParameter("avt");
+//start get data from Profile.jsp 
+        Part file_avt = request.getPart("avt");
         String fullname = request.getParameter("fullname") == null ? "" : request.getParameter("fullname");
-        String email = request.getParameter("email") == null ? "" : request.getParameter("email");
+        //String email = request.getParameter("email") == null ? "" : request.getParameter("email");
         String gender = request.getParameter("gender") == null ? "" : request.getParameter("gender");
         boolean bool_gender = gender.equals("male");
 
@@ -94,15 +103,27 @@ public class profileServlet extends HttpServlet {
         String old_password = request.getParameter("old_password") == null ? "" : request.getParameter("old_password");
         String new_password = request.getParameter("new_password") == null ? "" : request.getParameter("new_password");
         String re_new_password = request.getParameter("re_new_password") == null ? "" : request.getParameter("re_new_password");
+//end get data from Profile.jsp
 
+        AccountDAO accountDAO = new AccountDAO();
         Profile new_profile = (Profile) session.getAttribute("profile");
         Account new_account = (Account) session.getAttribute("account");
+
+        if (file_avt != null && file_avt.getSize() > 0) {
+            String avt_file_name = createFileNameRandom(file_avt);
+            //insert selectted_image into server (not build)
+            //String test_path = 
+            insertImageIntoServer(request, avt_file_name, file_avt);
+            String avt_path_in_server = "images/" + avt_file_name;
+            new_profile.setAvt(avt_path_in_server);
+            accountDAO.updateAvatar_ByAccId(avt_path_in_server, new_account.getAccount_id());
+            //o.print(test_path);
+        }
 
         if (fullname.isBlank()) {
             request.setAttribute("error", "You must have a name!");
         } else {
             //update db profile name
-            AccountDAO accountDAO = new AccountDAO();
             Account account = (Account) session.getAttribute("account");
             accountDAO.updateFullName_ByAccId(fullname, account.getAccount_id());
             new_profile.setFullname(fullname);
@@ -128,7 +149,6 @@ public class profileServlet extends HttpServlet {
 
         if (!gender.equals("")) {
             //update gender vao db profile
-            AccountDAO accountDAO = new AccountDAO();
             Account account = (Account) session.getAttribute("account");
             accountDAO.updateGender_ByAccId(bool_gender, account.getAccount_id());
             new_profile.setGender(bool_gender);
@@ -149,7 +169,6 @@ public class profileServlet extends HttpServlet {
                         request.setAttribute("error", "Password must be at least 8 characters!");
                     } else {
                         //update password
-                        AccountDAO accountDAO = new AccountDAO();
                         Account account = (Account) session.getAttribute("account");
                         accountDAO.updatePassword_ByAccId(new_password, account.getAccount_id());
                         new_account.setPassword(new_password);
@@ -169,7 +188,6 @@ public class profileServlet extends HttpServlet {
         session.setMaxInactiveInterval(60 * 30);
 
         request.getRequestDispatcher("Profile.jsp").forward(request, response);
-
     }
 
     /**
@@ -181,5 +199,39 @@ public class profileServlet extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
+    private String createFileNameRandom(Part file_avt) {
+        String image_file_name = file_avt.getSubmittedFileName();
+        String[] image_file_name_split = image_file_name.split("\\.");
+
+        image_file_name_split[0] = image_file_name_split[0] + (int) (Math.random() * 10000);
+        image_file_name = image_file_name_split[0] + "." + image_file_name_split[1];
+
+        return image_file_name;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    private String insertImageIntoServer(HttpServletRequest request, String avt_file_name, Part file_avt) {
+        String upload_directory = "/images"; // folder goc cua web khi builded
+        //tra ve folder khi not_build
+        String upload_path = request.getServletContext().getRealPath(upload_directory).replaceFirst("build", "") + File.separator + avt_file_name;
+        String replacedPath = upload_path.replace("\\", "/");
+        String replacePath_not_build = replacedPath.replaceFirst("//", "/");
+        try {
+            FileOutputStream fos = new FileOutputStream(replacePath_not_build);
+            InputStream is = file_avt.getInputStream();
+
+            byte[] data = new byte[is.available()];
+            is.read(data);
+            fos.write(data);
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return replacePath_not_build;
+    }
 
 }
