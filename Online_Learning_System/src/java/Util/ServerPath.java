b5dd17ec864f58d4b83a5aad306f714535ff98ad
C4tTu0n9 <tuongpche171654@fpt.edu.vn>
2024-06-28 18:23:32 +0700

    TuongPCHE171654 modified name folder_final

---
+++ Online_Learning_System/src/java/Util/ServerPath.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Util;
+
+/**
+ *
+ * @author tuong
+ */
+public class ServerPath {
+    
+    public static String getPathAvatar(){
+        return "E:\\Project_E-Learning\\V05.1.2_MergeCodeComentAndWishList\\web\\images";
+    }
+     public static String getPathImageCouse(){
+        return "E:\\Project_E-Learning\\V05.1.2_MergeCodeComentAndWishList\\web\\image_course";
+    }
+}
